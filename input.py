534e543875225cc5afaecebe18c89d83af2d692b
Mehdi Heyderov <mehdiheyderov@MacBook-Pro-Mehdi.local>
2019-08-06 15:16:30 +0400

    cc jkasmc ka

---
+++ input.py
@@ -0,0 +1,16 @@
+name = input("Adiniz: ")
+surname = input("Soyadiniz: ")
+
+
+
+print("**********")
+print("salam " + name + " " + surname)
+print("**********")
+print("ne qeder pul lazimdir?")
+pull = input("mebleq: ")
+amons = int(pull)
+limit = 900
+print("Pulu vere bilirik? " + str(limit > amons))
+
+
+print("dsfafdwssfd")
